1c04bd4d1c14dc17a424f5e7f0cca6c067285ec0
Isaiah Iruoha <Siruoha@gmail.com>
2024-03-05 14:54:07 -0500

    changed file name

--- start_gunicorn.py
+++ start_gunicorn.py
@@ -2,7 +2,7 @@ import os
 from subprocess import call
 
 # Change directory to the API folder
-os.chdir('API')
+os.chdir('api')
 
 # Start gunicorn with your application
 call(['gunicorn', 'backend:app'])
